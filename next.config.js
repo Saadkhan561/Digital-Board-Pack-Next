37fef6dc259ff48e0b2547be77f229c74f2bbe8b
AliyanAamir <aliyan.ashraf46@gmail.com>
2024-08-07 04:53:19 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
 
   images: {
-    domains: ["950a-39-34-147-203.ngrok-free.app", "localhost"],
+    domains: ["950a-39-34-147-203.ngrok-free.app", "localhost","aliyan20093.bsite.net"],
   },
   async redirects() {
     return [
       {
         source: "/pdf/:parentName/:docName",
         // destination: "https://950a-39-34-147-203.ngrok-free.app/document/:parentName/:docName",
-        destination: "http://localhost:7077/document/:parentName/:docName",
+        destination: "https://aliyan20093.bsite.net/document/:parentName/:docName",
         permanent: false,
       },
     ];
